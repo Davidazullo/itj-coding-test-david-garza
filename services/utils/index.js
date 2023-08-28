b73485fe35d1c45419e85e62dc7214762198d80b
David Andrés Garza Medina <git@davidgarza.me>
2023-08-28 00:21:21 -0600

    Sanitize input

--- services/utils/index.js
+++ services/utils/index.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 
 function get_number_array_from_file(filepath) {
-    const array = fs.readFileSync(filepath).toString().split(',');
+    const sanitized_input = fs.readFileSync(filepath).toString().replace(/[^0-9,]/g, '');
+    const array = sanitized_input.split(',').filter(x => x !== '');
     return array;
 }
 
